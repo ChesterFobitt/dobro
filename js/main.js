d3df520512d612de60e9cbe27704b3d842d9df9d
filippovskiy <chesterfobitt@yandex.ru>
2014-05-01 13:31:18 +0400

    js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,12 @@
 $(function() {
-			var h = $('.i-header'),
-				l = $('.e-logo'),
-				m = $('.b-menu-horiz'),
-				i = $('.b-info'),
-				im = $('.e-logo a img');
+	headerFix = $('.b-header')
 	$(window).scroll(function() {
 		if(
 			$(this).scrollTop() > 1) {
-				h.css({'position' : 'fixed', 'top':'0', 'height':'52px', '':''});
-				l.css({'marginTop' : '5px'});
-				im.css({'width' : '186px'});
-				m.css({'marginTop':'12px'});
-				i.fadeOut(2);
+				headerFix.addClass('fixed-header')
 			 } 
 		else {
-			h.css({'position' : 'relative', 'marginTop':'0', 'height':'90px'});
-			l.css({'marginTop' : '16px'});
-			im.css({'width' : '253px'});
-			m.css({'marginTop':'0'})
-			i.fadeIn(2);
-
+				headerFix.removeClass('fixed-header')
 		}
 	});
 });
